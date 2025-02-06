d54b6d3e7cdc0d3a2cc3f1e4261e12fe1090245d
Hayden <hayden.clark@mymail.champlain.edu>
2025-02-06 18:18:07 -0500

    Add example  route

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express, { type Request, type Response } from "express";
+import announcementsRoute from "./routes/announcements"
+const app = express()
+
+app.get("/", (request: Request, response: Response) => {
+    response.set("content-type","text/plain")
+    response.status(200).send("Champlain API");
+})
+
+
+app.use("/announcements", announcementsRoute)
+
+app.listen(3000, () => {
+    console.log("API running on :3000!")
+})
