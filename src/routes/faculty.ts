8b0c51297cc45e32f07876234a5ab49c687efa0d
Jack Nordberg <143552881+jnord0@users.noreply.github.com>
2025-02-13 19:04:02 -0500

    Update faculty.ts

--- src/routes/faculty.ts
+++ src/routes/faculty.ts
@@ -1,19 +1,11 @@
 import express from "express";
 import type { Response, Request } from "express";
-import { fileURLToPath } from "url";
 import fs from "fs";
+import type { Faculty } from "../types/faculty.d.ts"
 
-const facultyJSON = JSON.parse(fs.readFileSync("src/data/faculty.json", "utf8"));
+const facultyJSON: Faculty[] = JSON.parse(fs.readFileSync("src/data/faculty.json", "utf8"));
 
 const router = express.Router();
-
-// Define the Faculty type
-interface Faculty {
-    name: string;
-    title: string;
-    departments: string[];
-    imageUrl: string;
-}
 
 // Initialize an empty array for faculty data
 let facultyData: Faculty[] = facultyJSON; // Populate the array
@@ -24,7 +16,6 @@ router.get("/", async (req: Request, res: Response) => {
     res.json(facultyData);
  });
  
-
 // Route to get a specific faculty member by name
 router.get("/:name", async (req: Request, res: Response) => {
     res.setHeader("Content-Type", "application/json");
@@ -41,12 +32,10 @@ router.get("/:name", async (req: Request, res: Response) => {
 
 // Route to get faculty members by department
 router.get("/department/:departmentName", async (req: Request, res: Response) => {
-    console.log("Requested department:", req.params.departmentName);  // Log department name from the URL
     res.setHeader("Content-Type", "application/json");
 
     // Normalize the department name
     const departmentName = req.params.departmentName.trim().toLowerCase();
-    console.log("Normalized department name:", departmentName); // Log normalized department name
 
     const filteredFaculty = facultyData.filter(faculty =>
         // Ensure it's comparing case-insensitive and handling potential extra spaces
