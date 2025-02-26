df90a3ef8da0a19d7cb9cc5efbce7629cc39cba4
Hayden <132246021+hayden36@users.noreply.github.com>
2025-02-25 19:17:22 -0500

    Add prisma and update shuttle and announcement endpoints (#35)
    This PR adds prisma. It also converts the `/shuttles` and
    `/announcement` endpoints to fetch data from the database. Example data
    can be added to the database using the `seed.ts` file. This also adds
    the ability to filter announcements by type.
    
    This does not introduce any authentication on the endpoints.

---
+++ api/prisma/seed.ts
@@ -0,0 +1,41 @@
+import prisma from "../src/prisma_client"
+
+async function addSeedData() {
+    // Add example announcements
+    await prisma.announcement.upsert({
+        where: {id: 1},
+        update: {},
+        create: {
+            title: "Shuttle service suspended",
+            description: "The shuttle service is suspended until further notice.",
+            style: 'INFO',
+            type: ["SHUTTLE"]
+        }
+    })
+    await prisma.announcement.upsert({
+        where: {id: 2},
+        update: {},
+        create: {
+            title: "Campus Closure",
+            description: "The campus is closed until further notice due to the impending snow storms. Please be safe and stay off the roads!",
+            style: 'EMERGENCY',
+            type: ["WEB", "MOBILE"]
+        }
+    })
+
+    // Add example shuttle.
+    await prisma.shuttle.upsert({
+        where: {id: 1},
+        update: {},
+        create: {
+            direction: 0,
+            lat: 44.47394871475691,
+            lon: -73.20577978477449,
+            mph: 20,
+            updated: new Date(Date.now())
+        }
+    })
+
+}
+
+addSeedData()
