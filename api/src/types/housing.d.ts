9f02daf5ab4c05061c856d13dead63e35d049e10
Jack Nordberg <143552881+jnord0@users.noreply.github.com>
2025-03-29 17:34:54 -0400

    Add files via upload

---
+++ api/src/types/housing.d.ts
@@ -0,0 +1,13 @@
+/**
+ * @description This is the housing object.
+ * All the Info that is being returned.
+ */
+
+export type Housing = {
+    name: string;
+    type: string;
+    students: string;
+    distance: string;
+    address: string;
+    imageUrl: string;
+}
