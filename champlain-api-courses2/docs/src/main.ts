a750a441254434e14e1762b98677f68b51a79f61
CalebLink18 <143741278+CalebLink18@users.noreply.github.com>
2025-04-02 13:59:14 -0400

    Delete champlain-api-courses2 directory
    was inserted on accident

--- champlain-api-courses2/docs/src/main.ts
+++
@@ -1,8 +0,0 @@
-import SwaggerUI from "swagger-ui"
-import "swagger-ui/dist/swagger-ui.css"
-import docYAML from "/swagger-config.yaml?url"
-
-SwaggerUI({
-  dom_id: '#app',
-  url: `${docYAML}`
-})
