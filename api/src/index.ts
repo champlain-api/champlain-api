43ae4b1ac5064309e31b6388ba6344d49ec0a40e
Jack Nordberg <143552881+jnord0@users.noreply.github.com>
2025-04-11 17:24:59 -0400

    route for housing endpoint

--- api/src/index.ts
+++ api/src/index.ts
@@ -19,6 +19,7 @@ import announcementsRoute from "./routes/announcements.ts"
 import shuttleRoute from "./routes/shuttle.ts"
 import facultyRoute from "./routes/faculty.ts"
 import rootRoute from "./routes/root.ts"
+import housingRoute from "./routes/housing.ts"
 
 const app = express()
 
@@ -26,6 +27,7 @@ app.use("/", rootRoute)
 app.use("/announcements", announcementsRoute)
 app.use("/shuttles", shuttleRoute)
 app.use("/faculty", facultyRoute)
+app.use("/housing", housingRoute)
 
 app.listen(3000, () => {
     console.log("API running on :3000!")
