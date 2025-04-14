8bdf9866a2d5cceb494ae16507d4cfdf80d54365
Jack Nordberg <143552881+jnord0@users.noreply.github.com>
2025-04-14 12:58:22 -0400

    Update migration.sql

--- api/prisma/migrations/20250411180510_adding_housing/migration.sql
+++ api/prisma/migrations/20250411180510_adding_housing/migration.sql
@@ -15,7 +15,7 @@ CREATE TABLE "Housing" (
     "updated" TIMESTAMPTZ(6) NOT NULL,
     "name" TEXT NOT NULL DEFAULT '',
     "type" TEXT NOT NULL DEFAULT '',
-    "students" TEXT NOT NULL DEFAULT '',
+    "students" INTEGER NOT NULL DEFAULT 0,
     "distance" TEXT NOT NULL DEFAULT '',
     "address" TEXT NOT NULL DEFAULT '',
     "imageURL" TEXT NOT NULL DEFAULT '',
