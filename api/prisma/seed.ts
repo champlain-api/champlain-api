eb31d753091f3ef3957c937ec48f267b6767a6df
Jack Nordberg <143552881+jnord0@users.noreply.github.com>
2025-04-14 13:03:01 -0400

    Update seed.ts

--- api/prisma/seed.ts
+++ api/prisma/seed.ts
@@ -78,7 +78,7 @@ async function addSeedData() {
         data: {
             name: "South House",
             type: "Victorian-Era Mansion",
-            students: "47",
+            students: 47,
             distance: "2 blocks",
             address: "363 S. Willard St, Burlington, VT 05401",
             imageURL: "https://www.champlain.edu/app/uploads/2023/08/South-House-2-800x450.jpg"
@@ -89,7 +89,7 @@ async function addSeedData() {
         data: {
             name: "Summit Hall",
             type: "Victorian-Era Mansion",
-            students: "48",
+            students: 48,
             distance: "2 blocks",
             address: "322 Maple St, Burlington, VT 05401",
             imageURL: "https://www.champlain.edu/app/uploads/2023/08/Summit-Hall_res-hall-dorm-residence-2-800x450.jpg"
